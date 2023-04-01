41a6d26e3c255436b8c5a2f8c4391e4c8c14c02f
glbdmn <gabrieledamiani@hotmail.it>
2023-04-01 16:23:22 +0200

    BONUS: Aggiunta select livelli

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,23 +15,57 @@ document.getElementById('bottone').addEventListener('click',function() {
 
     let griglia = document.getElementById("griglia");
     griglia.innerHTML = "";
+    let livello = document.getElementById('livello').value;
+    console.log ("il livello è " + livello);
 
- for (let i = 1; i <= 100; i++) {
-    let divbox = creazioneElemento("div", "item");
-     divbox.addEventListener("click", function() {
-        this.classList.toggle('active');
-     })
+    // heasy = 1
+    if (livello == 1) {     
+        for (let i = 1; i <= 100; i++) {
+
+            let divbox = creazioneElemento("div", "item");
+
+            divbox.addEventListener("click", function() {
+                this.classList.toggle('active');
+            })
+
+            divbox.append(i);
+            griglia.append(divbox);
+    
+            }
+    } 
+    if (livello == 2) {     
+        for (let i = 1; i <= 81; i++) {
+
+            let divbox = creazioneElemento("div", "item");
+
+            divbox.addEventListener("click", function() {
+                this.classList.toggle('active');
+            })
+
+            divbox.append(i);
+            griglia.append(divbox);
+    
+            }
+    } 
+    if (livello == 3) {     
+        for (let i = 1; i <= 49; i++) {
+
+            let divbox = creazioneElemento("div", "item");
+
+            divbox.addEventListener("click", function() {
+                this.classList.toggle('active');
+            })
+
+            divbox.append(i);
+            griglia.append(divbox);
+    
+            }
+    } 
 
 
-   
-    divbox.append(i);
-    griglia.append(divbox);
 
 
-}
-
-
- })
+})
 
 
 
